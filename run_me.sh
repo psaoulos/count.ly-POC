51502d4d161f9b9a7a7dd3f5024d98a0c52641a1
Pantelhs <psaoylos@gmail.com>
2022-04-18 14:40:56 +0300

    Added innitial demo web app / backend

--- run_me.sh
+++ run_me.sh
@@ -12,9 +12,15 @@ export GROUP_ID=$(id -g);
 #     source .env
 # fi
 
-echo "Starting count.ly Server!"
-cd Server
-docker container stop $(docker container ls -aq)
+# cd demo_web_app
+# npm i
+# npm run build
+# cd ../demo_web_app_backend
+# npm i
+# cd ..
+
+echo "Starting Containers!"
+# docker container stop $(docker container ls -aq)
 # docker rm $(docker ps -a -q)
 
 docker-compose -f docker-compose.yml build && COUNTLY_CONFIG_HOSTNAME='localhost' docker-compose -f docker-compose.yml up
